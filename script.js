995ecb24847c5843dbf3daeb682edcdbcc00b604
millerwarren <53824450+millerwarren@users.noreply.github.com>
2019-08-13 10:21:18 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+var number = prompt("Pick a number!");
+
+if (number == 7) {
+    alert("You got is right!");
+} else if (number > 7) {
+    alert("Too high, guess again!");
+} else if (number < 7) {
+    alert("Too low, guess again!");
+}
